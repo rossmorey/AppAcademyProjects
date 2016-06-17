a523ae9e45684235d072131ec0af7a773ce5a91c
Ross Morey <ross.morey@gmail.com>
2016-06-17 16:40:28 -0700

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def update
