eb3344f13b5cdf29c02690f7aeba0c90632134ae
rossmorey <ross.morey@gmail.com>
2016-07-15 12:33:05 -0700

    made most of KnightPathFinder

---
+++ KnightPathFinder.rb
@@ -0,0 +1,58 @@
+require_relative "PathNode.rb"
+
+class KnightPathFinder
+
+  BOARD_LENGTH = 8
+
+  SHIFT = [
+    [1, -2],
+    [2, -1],
+    [2, 1],
+    [1, -2],
+    [-1, 2],
+    [-2, -1],
+    [-2, 1],
+    [-1, 2]
+  ]
+
+  attr_reader :position
+
+  def initialize(position)
+    @position = position
+    @visited_positions = [position]
+
+    # build_move_tree
+  end
+
+  def build_move_tree(start_pos)
+    root = PathNode.new(start_pos)
+    nodes = [root]
+    until nodes.empty?
+      current_node = nodes.shift
+      new_move_positions(current_node.value).each do |new_position|
+        new_node = PathNode.new(new_position)
+        new_node.parent = current_node
+        nodes << new_node
+      end
+    end
+    root
+  end
+
+  def new_move_positions(pos)
+    valid_moves = KnightPathFinder.valid_moves(pos)
+    new_valid_moves = valid_moves.select do |move|
+      !@visited_positions.include?(move)
+    end
+    @visited_positions.concat(new_valid_moves)
+    new_valid_moves
+  end
+
+  def self.valid_moves(pos)
+    SHIFT.map do |shift|
+      [shift.first + pos.first, shift.last + pos.last]
+    end.select do |pos|
+      pos.all? { |num| num.between?(0, 7) }
+    end
+  end
+
+end
