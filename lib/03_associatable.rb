c9de578006aca787e31e5115a366f39c9b969c2e
Ross Morey <ross.morey@gmail.com>
2016-08-10 18:41:46 -0700

    Almost there -- just implementing has_one_through

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -3,8 +3,6 @@ require 'active_support/inflector'
 
 # Phase IIIa
 class AssocOptions
-  # extend BelongsToOptions
-
   attr_accessor(
     :foreign_key,
     :class_name,
@@ -12,59 +10,76 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-
+    "#{class_name.to_s.downcase}s"
   end
 end
 
 class BelongsToOptions < AssocOptions
-attr_reader :class_name, :foreign_key, :primary_key
 
   def initialize(name, options = {})
-    if options.empty?
-      @class_name = name.capitalize.singularize
-      @foreign_key = "#{name.singularize.underscore}_id".to_sym
-      @primary_key = :id
-    else
-      @class_name = options[:class_name]
-      @foreign_key = options[:foreign_key]
-      @primary_key = options[:primary_key]
+    defaults = {
+      class_name: name.to_s.capitalize.singularize,
+      foreign_key: "#{name.to_s.singularize.underscore}_id".to_sym,
+      primary_key: :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key] )
     end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    if options.empty?
-      @class_name = name.capitalize.singularize
-      @foreign_key = "#{self_class_name.singularize.underscore}_id".to_sym
-      @primary_key = :id
-    else
-      @class_name = options[:class_name]
-      @foreign_key = options[:foreign_key]
-      @primary_key = options[:primary_key]
+    defaults = {
+      class_name: name.to_s.capitalize.singularize,
+      foreign_key: "#{self_class_name.to_s.singularize.underscore}_id".to_sym,
+      primary_key: :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
     end
   end
 end
 
 module Associatable
   # Phase IIIb
+  # attr_reader :has_many_options, :belongs_to_options
+
   def belongs_to(name, options = {})
-    # ...
+    assoc_options[name] = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+      target = options.model_class
+      fkey_value = self.send(options.foreign_key)
+
+      target.where((options.primary_key).to_sym => fkey_value).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    assoc_options[name] = HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+      target = options.model_class
+      pkey_value = self.send(options.primary_key)
+
+      target.where((options.foreign_key).to_sym => pkey_value)
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -5,6 +5,43 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    # debugger
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      target = through_options.model_class
+      source_options = target.assoc_options[source_name]
+
+      pkey_value = self.send(through_options.primary_key)
+
+      start_table = self.class.table_name
+      through_table = through_options.table_name
+      source_table = source_options.table_name
+
+      values = []
+      values << "#{through_table}"
+      values << "#{source_table}"
+      values << "#{source_table}.#{source_table.primary_key} = #{through_table}.#{through_table.foreign_key}"
+      values <<  "#{start_table}"
+      values << "#{start_table}.#{start_table.primary_key} = #{through_table}.#{through_table.foreign_key}"
+      values << "#{start_table}.id = #{pkey_value}"
+
+      results = DBConnection.execute(<<-SQL, *values)
+        SELECT
+          *
+        FROM
+          ?
+        JOIN ?
+          ON ? = ?
+        JOIN ?
+          ON ? = ?
+        WHERE
+          ?
+      SQL
+
+      results
+      # pkey_value = self.send(options.primary_key)
+      #
+      # target.where((options.foreign_key).to_sym => pkey_value)
+    end
   end
 end
