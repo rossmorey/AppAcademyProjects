ba50841925a8dff5b7ea866dbafe15a013d4d4ad
rossmorey <ross.morey@gmail.com>
2016-08-19 15:56:14 -0700

    Finished .ajax

--- testdata.js
+++ testdata.js
@@ -17,3 +17,8 @@ let li = $l('.element')
 ul.append("<li>Hello World</li>")
 
 let otherUL= $l('.test')
+
+$l.ajax({
+  url:'http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  success: function(message){ console.log(message);}
+})
